58a989f03f4d85ee98ebc688faed36fa7df878ba
theripeer <cristianandrescv@gmail.com>
2020-09-27 13:12:05 -0300

    Creados estilos de index | Maquetacion de index

--- api/index.php
+++ api/index.php
@@ -4,11 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DOC API-SPHERE</title>
+    <link rel="stylesheet" href="assets/estilos.css">
 </head>
 <body>
     <div class="container">
-        <h1>AUTH</h1>
-        <hr>
+        <div class="divbody">
+            <h3>Auth - login</h3>
+            <code>
+                POST  /auth
+                    <br>
+                {
+                    <br>
+                    "usuario" :"",  -> REQUERIDO
+                    <br>
+                    "password": "" -> REQUERIDO
+                    <br>
+                }
+            
+            </code>
+        </div>      
     </div>
 </body>
 </html>
